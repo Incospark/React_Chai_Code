a01e09b4dbc6931b0c3b0ac397cbc2b1c93afd6a
Incospark <Incospark>
2026-03-04 00:06:20 +0530

    updated

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState, useCallback } from 'react'
+
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(true)
+  const [password, setPassword] = useState("")
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "~!@#$%^&*()+_-={}[]"
+
+    for
+
+  
+
+
+
+
+  }, [length, numberAllowed, charAllowed, setPassword]) 
+
+  return (
+    <>
+      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+    </>
+  )
+}
+
+export default App
