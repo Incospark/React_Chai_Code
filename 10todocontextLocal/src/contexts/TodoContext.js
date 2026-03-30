3cfe307c2f76eb63cd463100c38e03af94067664
Incospark <Incospark>
2026-03-30 12:59:55 +0530

    incomplete

---
+++ 10todocontextLocal/src/contexts/TodoContext.js
@@ -0,0 +1,23 @@
+import { createContext, useContext } from "react";
+
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            title: "Learn React",
+            completed: false,
+        }
+    ],
+    addTodo: (title) => { },
+    updateTodo: (id, title) => { },
+    deleteTodo: (id) => { },
+    toggleCompleted: (id) => { }
+});
+
+
+export const useTodoContext = () => {
+    return useContext(TodoContext);
+}
+
+export const TodoProvider = TodoContext.Provider;
